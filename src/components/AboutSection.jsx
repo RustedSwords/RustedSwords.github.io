c0e752b6b4dbc0889c83b8a305cafc78aadbd971
RustedSwords <manavmakwana27@gmail.com>
2026-01-10 17:42:16 +0100

    style changes

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,4 +1,4 @@
-import { Briefcase, Code, User } from "lucide-react";
+import { Languages, BrainCircuit, Database } from "lucide-react";
 
 export const AboutSection = () => {
   return (
@@ -49,7 +49,7 @@ export const AboutSection = () => {
             <div className="gradient-border p-6 card-hover">
               <div className="flex items-start gap-4">
                 <div className="p-3 rounded-full bg-primary/10">
-                  <Code className="h-6 w-6 text-primary" />
+                  <BrainCircuit className="h-6 w-6 text-primary" />
                 </div>
                 <div className="text-left">
                   <h4 className="font-semibold text-lg"> Machine Learning</h4>
@@ -63,7 +63,7 @@ export const AboutSection = () => {
             <div className="gradient-border p-6 card-hover">
               <div className="flex items-start gap-4">
                 <div className="p-3 rounded-full bg-primary/10">
-                  <User className="h-6 w-6 text-primary" />
+                  <Database className="h-6 w-6 text-primary" />
                 </div>
                 <div className="text-left">
                   <h4 className="font-semibold text-lg">Data Science</h4>
@@ -77,7 +77,7 @@ export const AboutSection = () => {
             <div className="gradient-border p-6 card-hover">
               <div className="flex items-start gap-4">
                 <div className="p-3 rounded-full bg-primary/10">
-                  <Briefcase className="h-6 w-6 text-primary" />
+                  <Languages className="h-6 w-6 text-primary" />
                 </div>
 
                 <div className="text-left">
